efd523f5264d5f8ed2dbb767a7df7bf42235f45c
sboschraga <sboschraga@gmail.com>
2026-01-04 20:47:32 +0100

    canvis al catàleg i tipos

--- client/src/pages/Denegat.jsx
+++ client/src/pages/Denegat.jsx
@@ -1,4 +1,3 @@
-// src/pages/Denegat.jsx
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
 
@@ -6,21 +5,16 @@ function Denegat() {
   const location = useLocation();
   const distance = location.state?.distance || "?"; 
 
-  // CANVI 4: Centrat absolut i fons blanc (igual que la barra)
+  // AQUEST ESTIL ARA ÉS IGUAL QUE EL D'ACCIONS
+  // Sense 'absolute' ni 'zIndex', així respecta la barra de dalt.
   const containerStyle = {
-    display: "flex",         
-    flexDirection: "column", 
-    justifyContent: "center", // Centra en vertical (Eix Y)
-    alignItems: "center",     // Centra en horitzontal (Eix X)
-    height: "100vh",          // Ocupa tota l'alçada de la pantalla
-    width: "100vw",           // Ocupa tota l'amplada
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "80vh", // Mateixa alçada que Accions perquè es vegi igual
     textAlign: "center",
-    padding: "20px",
-    backgroundColor: "white", // Fons blanc per igualar la barra superior
-    position: "absolute",     // Assegura que cobreixi tot
-    top: 0,
-    left: 0,
-    zIndex: 10                // Posa-ho per sobre si cal
+    padding: "20px"
   };
 
   return (
@@ -28,9 +22,8 @@ function Denegat() {
       <h1 style={{ color: "red", fontSize: "2rem", textTransform: "uppercase" }}>
         Accés Denegat!
       </h1>
-      
-      <p style={{ fontSize: "1.2rem", fontFamily: "monospace", maxWidth: "600px", margin: "20px 0" }}>
-        ves a un cul de sac per a poder accedir al nostre catàleg.
+      <p style={{ fontSize: "1.2rem", maxWidth: "600px", margin: "20px 0" }}>
+          ves a un cul de sac per a poder accedir al nostre catàleg.
         <br /><br />
         (estàs a <strong>{distance} metres</strong> del cul de sac registrat més proper)
       </p>
