fc04a7d3dd575f80d61521d62d0bef5e49ff6307
sboschraga <sboschraga@gmail.com>
2026-01-06 11:45:49 +0100

    fotos afegides

--- client/src/data/infoCarrers.js
+++ client/src/data/infoCarrers.js
@@ -4,403 +4,409 @@ const infoCarrers = {
   "La Rambla": {
     lat: 41.380775,
     lon: 2.173661,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🚶", "🌳", "🎭", "☕"]
   },
   "Plaça Urquinaona": {
     lat: 41.388480,
     lon: 2.174390,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏙️", "🚇", "🚌", "🐦"]
   },
   "Carrer Moianès": {
     lat: 41.6141373,
     lon: 2.2893226,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏘️", "🚗", "🏠", "🛒"]
   },
   "Plaça de les Olles": {
     lat: 41.6084448,
     lon: 2.2872299,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏺", "🥘", "🍻", "🕯️"]
   },
   "C/ Rera Sant Just": {
     lat: 41.38253836691902,
     lon: 2.1777601558566126,
-    fotos: [null, null],
+    fotos: ["/img/Ciutat-vella/carrer-de-rera-sant-just_ciutat-vella.jpg", "/img/Ciutat-vella/carrer-de-rera-sant-just_2_ciutat-vella.jpg"],
     simbols: ["📍", "📜", "🏛️", "🧱", "⛪"]
   },
   "C/ de Sant Simplici": {
     lat: 41.38164065999463,
     lon: 2.1789911726284483,
-    fotos: [null, null],
+    fotos: ["/img/Ciutat-vella/carrer-de-sant-simplici_ciutat-vella.jpg", "/img/Ciutat-vella/carrer-de-sant-simplici_2_ciutat-vella.jpg"],
     simbols: ["📍", "🧱", "🛡️", "🕯️", "🕍"]
   },
   "C/ de Riudarenes": {
     lat: 41.3809908455344,
     lon: 2.1797510001495874,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🌊", "🧱", "🚪", "🌬️"]
   },
   "C/ del Tonell": {
     lat: 41.38071148583164,
     lon: 2.17991098173389,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🍷", "🍇", "🪵", "🏷️"]
   },
   "C/ de la Tarongeta": {
     lat: 41.38333689326777,
     lon: 2.181049643017582,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🍊", "🌳", "🍃", "☀️"]
   },
   "C/ de Nicolau de Sant Climent": {
     lat: 41.38372377336681,
     lon: 2.18092523534211,
-    fotos: [null, null],
+    fotos: ["/img/Ciutat-vella/carrer-de-nicolau-de-sant-climent_ciutat-vella.jpg", "null"],
     simbols: ["📍", "🎩", "📜", "🏰", "⚖️"]
   },
   "Ptge. de l’Oli": {
     lat: 41.38486990940606,
     lon: 2.1785662819454026,
-    fotos: [null, null],
+    fotos: ["/img/Ciutat-vella/carrer-de-loli_ciutat_vella.jpg", "/img/Ciutat-vella/carrer-de-loli_2_ciutat-vella.jpg"],
     simbols: ["📍", "🫒", "🏺", "🍳", "🥖"]
   },
   "C/ de la Volta de la Perdiu": {
     lat: 41.386606008492606,
     lon: 2.176915928572149,
-    fotos: [null, null],
+    fotos: ["/img/Ciutat-vella/carrer-de-la-volta-de-la-perdiu_ciutat vella.jpg", "null"],
     simbols: ["📍", "🐦", "🌾", "🏘️", "🔒"]
   },
   "Ptge. de Lucà": {
     lat: 41.41052575307892,
     lon: 2.139402273531987,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/passatge-de-luca_sarria_sant-gervasi.jpg", "null"],
     simbols: ["📍", "🛣️", "🌲", "🚵", "🏞️"]
   },
   "Ptge. del Putxet": {
     lat: 41.404059059346295,
     lon: 2.1426867923084196,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/passatge-del-putxet_sarria_sant-gervasi.jpg", "/img/Sarrià-Sant Gervàsi/passatge-del-putxet_2_sarria_sant-gervasi.jpg"],
     simbols: ["📍", "🏞️", "🔭", "🌳", "🌄"]
   },
   "C/ de Felip Gil": {
     lat: 41.40718403377116,
     lon: 2.143423553734635,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-de-felip-gil_sarria_sant-gervasi.jpg", "/img/Sarrià-Sant Gervàsi/carrer-de-felip-gil_2_sarria_sant-gervasi.jpg"],
     simbols: ["📍", "🏠", "🧱", "🔑", "🚪"]
   },
   "C/ del Francolí": {
     lat: 41.40457692270555,
     lon: 2.147912951761781,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-francoli_sarria_sant-gervasi.jpg", "/img/Sarrià-Sant Gervàsi/carrer-francoli_2_sarria_sant-gervasi.jpg"],
     simbols: ["📍", "🐟", "🌊", "🎣", "🛶"]
   },
   "Carreró de les Carolines": {
     lat: 41.403999323920615,
     lon: 2.151093767983999,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🌺", "👒", "🏠", "🧱"]
   },
   "Ptge. de Sant Josep de la Muntanya": {
     lat: 41.411051638905015,
     lon: 2.1544306190168947,
-    fotos: [null, null],
+    fotos: ["/img/Gràcia/passatge-de-josep-de-la-muntanya_gracia.jpg", "/img/Gràcia/passatge-de-josep-de-la-muntanya_2_gracia.jpg"],
     simbols: ["📍", "⛪", "⛰️", "🛤️", "🛐"]
   },
   "C/ de Sant Cugat del Vallès": {
     lat: 41.411355377738964,
     lon: 2.1535206226931103,
-    fotos: [null, null],
+    fotos: ["/img/Gràcia/carrer-de-sant-cugat-del-valles_gracia.jpg", "/img/Gràcia/carrer-de-sant-cugat-del-valles_2_gracia.jpg"],
     simbols: ["📍", "🤝", "🏙️", "🛣️", "🌉"]
   },
   "C/ de Logroño": {
     lat: 41.44050235020748,
     lon: 2.1570647898075523,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🍇", "🍷", "🇪🇸", "🛣️"]
   },
   "C/ de la Font Baliarda": {
     lat: 41.441346727669846,
     lon: 2.1557424633772713,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛲", "💧", "🌳", "🧊"]
   },
   "C/ dels Pirineus": {
     lat: 41.43844935629942,
     lon: 2.161210328689008,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛰️", "⛷️", "🌨️", "🦅"]
   },
   "C/ de la Rectoria": {
     lat: 41.434139161194324,
     lon: 2.159914709864333,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛪", "📜", "🖊️", "🎓"]
   },
   "C/ de Grifols": {
     lat: 41.427648709195665,
     lon: 2.1599186262859384,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "💉", "🩸", "🏥", "🧪"]
   },
   "Riera de Marcel·lí": {
     lat: 41.430253398550626,
     lon: 2.1529423440069113,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "💧", "🌊", "🌉", "🐸"]
   },
   "C/ d’Hug de Rocabertí": {
     lat: 41.3850596742807,
     lon: 2.133311956261469,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🛡️", "⚔️", "🏰", "👑"]
   },
   "Ptge. de Sant Ramon Nonat": {
     lat: 41.376321048721394,
     lon: 2.117987611974052,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🕊️", "⛪", "👶", "🕯️"]
   },
   "Ptge. de Vicent Martín": {
     lat: 41.37748096888126,
     lon: 2.116056310801696,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🎨", "🖌️", "🖼️", "🏠"]
   },
   "C/ d’Aiguallonga": {
     lat: 41.38873763161186,
     lon: 2.104924199147839,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "💧", "📏", "🏞️", "🌿"]
   },
   "Ptge. de Puig Aguilar": {
     lat: 41.388716371644335,
     lon: 2.104418884444168,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛰️", "🦅", "🌲", "🥾"]
   },
   "Pl. del Coll de Finestrelles": {
     lat: 41.38919651941972,
     lon: 2.1040843997175958,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🌄", "🛤️", "👀", "🔭"]
   },
   "C/ Costa de Sant Bartomeu de la Quadra": {
     lat: 41.38942377376603,
     lon: 2.104199517229367,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛪", "🐴", "🪵", "🌾"]
   },
   "Ptge. Carreras": {
     lat: 41.45683504455201,
     lon: 2.181884727189601,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏃", "🏁", "⏱️", "👟"]
   },
   "C/ de Palau-Solità": {
     lat: 41.45842468671127,
     lon: 2.182241610281811,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏰", "🏘️", "🧱", "🔑"]
   },
   "C/ de Ripollet": {
     lat: 41.45691656824185,
     lon: 2.182980139263495,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🐓", "🌾", "🚜", "🏙️"]
   },
   "C/ del Port de la Selva": {
     lat: 41.44705861908793,
     lon: 2.1789719081231955,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⚓", "⛵", "🌊", "🎣"]
   },
   "C/ del Verdet": {
     lat: 41.43847228336899,
     lon: 2.189590832442194,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-del-verdet-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-del-verdet-2_Sant-Andreu.jpg"],
     simbols: ["📍", "🟢", "🌿", "🍃", "🌳"]
   },
   "C/ de Tremp": {
     lat: 41.43869276227528,
     lon: 2.1877591976670163,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-de-tremp-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-de-tremp-2_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-de-tremp-3_Sant-Andreu.jpg"],
     simbols: ["📍", "🏔️", "🏞️", "💧", "🛤️"]
   },
   "C/ d'en Xandri": {
     lat: 41.43830655157453,
     lon: 2.185560476891055,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-den-Xandri-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-den-Xandri-2_Sant-Andreu.jpg"],
     simbols: ["📍", "🏠", "🧱", "🔑", "🚪"]
   },
   "Ptge. Baliarda": {
     lat: 41.436544364374875,
     lon: 2.188796995245255,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Passatge-Baliarda-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Passatge-Baliarda-2_Sant-Andreu.jpg"],
     simbols: ["📍", "💃", "🕺", "🎶", "🥁"]
   },
   "C/ Cerdà": {
     lat: 41.43565547293505,
     lon: 2.188819434514976,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-cerdà-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-cerdà-2_Sant-Andreu.jpg"],
     simbols: ["📍", "⛰️", "🌲", "🛤️", "🦌"]
   },
   "C/ de Gordi": {
     lat: 41.43565547293505,
     lon: 2.188819434514976,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-gordi-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-gordi-2_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-gordi-3_Sant-Andreu.jpg"],
     simbols: ["📍", "🧵", "🪡", "🧥", "👔"]
   },
   "Torrent de Parellada": {
     lat: 41.43220110347789,
     lon: 2.1880854347440897,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Torrent-de-parellada-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Torrent-de-parellada-1_Sant-Andreu.jpg"],
+    simbols: ["📍", "💧", "🌊", "🌉", "🐸"]
+  },
+  "C/ de Parellada": {
+    lat: 41.43220110347789,
+    lon: 2.1880854347440897,
+    fotos: ["/img/Sant-Andreu/Carrer-de-parellada-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-de-parellada-2_Sant-Andreu.jpg"],
     simbols: ["📍", "💧", "🌊", "🌉", "🐸"]
   },
   "C/ de les Monges": {
     lat: 41.432032998695256,
     lon: 2.1870421944893135,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "⛪", "🙏", "🕯️", "🕍"]
   },
   "Ptge. d'Irlanda": {
     lat: 41.43150959397617,
     lon: 2.18633448074808,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/ptge-dirlanda-1_Sant-Andreu.jpg", "/img/Sant-Andreu/ptge-dirlanda-2_Sant-Andreu.jpg"],
     simbols: ["📍", "🇮🇪", "🍀", "🍺", "🎻"]
   },
   "C/ dels Afores": {
     lat: 41.429528606195724,
     lon: 2.190764445532401,
-    fotos: [null, null],
+    fotos: ["/img/Sant-Andreu/Carrer-dels-afores-1_Sant-Andreu.jpg", "/img/Sant-Andreu/Carrer-dels-afores-2_Sant-Andreu.jpg"],
     simbols: ["📍", "🏙️", "🚜", "🛤️", "🚪"]
   },
   "Gran Via Catalana": {
     lat: 41.4297796166403,
     lon: 2.2036520656846474,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🛣️", "🚗", "🏙️", "🚦"]
   },
   "C/ del Pou de l'Alzina": {
     lat: 41.43266965724075,
     lon: 2.201631536994376,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🕳️", "💧", "🌳", "🌰"]
   },
   "C/ de Jaume Brossa": {
     lat: 41.430060226486056,
     lon: 2.19905496121926,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🚮", "♻️", "🍂", "🧹"]
   },
   "Ptge. de Malet": {
     lat: 41.41369671770875,
     lon: 2.188309264743962,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "💼", "👜", "🧳", "🚶"]
   },
   "C/ de l'Arc de Sant Sever": {
     lat: 41.409874618591765,
     lon: 2.18587008308247,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏹", "⛪", "🌉", "🧱"]
   },
   "C/ Meridional": {
     lat: 41.40782657976715,
     lon: 2.1870322448574115,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🧭", "⬇️", "☀️", "🌎"]
   },
   "C/ del Gaià": {
     lat: 41.36171825350059,
     lon: 2.1415116069624425,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🌊", "💧", "🏞️", "🛶"]
   },
   "C/ de Gabriel Miró": {
     lat: 41.353049599071994,
     lon: 2.1482761109055675,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "📖", "🖊️", "✍️", "📜"]
   },
   "Ptge. de Julià": {
     lat: 41.37014223182839,
     lon: 2.1613011152732984,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏠", "🏘️", "🚪", "🔑"]
   },
   "C/ de Cariteo": {
     lat: 41.37043362987127,
     lon: 2.1604464997175565,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏛️", "🇬🇷", "🎭", "🗿"]
   },
   "C/ de la Concòrdia": {
     lat: 41.37263698791773,
     lon: 2.1594698970000112,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🤝", "🕊️", "⚖️", "❤️"]
   },
   "C/ de Maria Victòria": {
     lat: 41.37296802453955,
     lon: 2.1377665013663845,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "👑", "👗", "👸", "🏰"]
   },
   "C/ de Pomar": {
     lat: 41.372234227430475,
     lon: 2.1346379065407217,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🍎", "🌳", "🍏", "🧺"]
   },
   "C/ de Demòstenes": {
     lat: 41.376259785732664,
     lon: 2.1335985029724776,
-    fotos: [null, null],
+    fotos: ["/img/Sants-Montuïc/carrer-de-demostens_sants_montjuic.jpg", "/img/Sants-Montuïc/carrer-de-demostens_2_sants_montjuic.jpg", "/img/Sants-Montuïc/carrer-de-demostens_3_sants_montjuic.jpg"],
     simbols: ["📍", "🗣️", "🏛️", "📜", "🇬🇷"]
   },
   "C/ de López Catalan": {
     lat: 41.3759601350936,
     lon: 2.1374172879565547,
-    fotos: [null, null],
+    fotos: ["/img/Sants-Montuïc/carrer-de-lopez-catalan_sants_montjuic.jpg", "/img/Sants-Montuïc/carrer-de-lopez-catalan_2_sants_montjuic.jpg"],
     simbols: ["📍", "💼", "👔", "📝", "🏢"]
   },
   "C/ dels Dominics": {
     lat: 41.40963557844661,
     lon: 2.1340033531724725,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-dels-dominics_sarria_sant-gervasi.jpg", "/img/Sarrià-Sant Gervàsi/carrer-dels-dominics_2_sarria_sant-gervasi.jpg"],
     simbols: ["📍", "⛪", "✝️", "📜", "🛐"]
   },
   "C/ de Bigai": {
     lat: 41.40566005248056,
     lon: 2.133749998405805,
-    fotos: [null, null],
+    fotos: ["null", "null"],
     simbols: ["📍", "🏠", "🛣️", "🔑", "🚪"]
   },
   "C/ de les Canàries": {
     lat: 41.40533168309466,
     lon: 2.1247430963679292,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-de-canaries_sarria_sant-gervasi.jpg", "/img/Sarrià-Sant Gervàsi/carrer-de-canaries_2_sarria_sant-gervasi.jpg"],
     simbols: ["📍", "🏝️", "🍌", "🌊", "☀️"]
   },
   "C/ de Margenat": {
     lat: 41.403586230370884,
     lon: 2.123442258686652,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-margenat_sarria_sant-gervasi.jpg", "null"],
     simbols: ["📍", "🧱", "📏", "🏗️", "📐"]
   },
   "C/ de l’Arquitecte Mas": {
     lat: 41.39934449380521,
     lon: 2.1209996214326345,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-de-larquitecta_sarria_sant-gervasi.jpg", "null"],
     simbols: ["📍", "🏗️", "📐", "🏠", "🏢"]
   },
   "C/ Pau Alcover": {
     lat: 41.40041764061599,
     lon: 2.128642166103607,
-    fotos: [null, null],
+    fotos: ["/img/Sarrià-Sant Gervàsi/carrer-de-pau-alcover_sarria_sant-gervasi.jpg", "null"],
     simbols: ["📍", "🧔", "👔", "📝", "🏢"]
   }
 };
